6f6bfe90ba5d96d06855742c0e9803f3fab3f353
afiiqkacak <66316306+afiiqkacak@users.noreply.github.com>
2020-09-06 16:58:14 +0800

    Update loginpage.php

--- loginpage.php
+++ loginpage.php
@@ -22,12 +22,12 @@
 				<form class="login100-form validate-form p-b-33 p-t-5" action="">
 
 					<div class="wrap-input100 validate-input" data-validate = "Enter username">
-						<input class="input100" type="text" name="ic" placeholder="IC No.">
+						<input class="input100" type="text" name="ic" placeholder="IC No." required>
 						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<input class="input100" type="password" name="pass" placeholder="Password" maxlength="8">
+						<input class="input100" type="password" name="pass" placeholder="Password" maxlength="8" required>
 						<span class="focus-input100" data-placeholder="&#xe80f;"></span>
 					</div>
 
